15113db8f031a8cf373d377dd137c109663c6a3c
dave griffiths <dave@fo.am>
2021-05-10 14:37:21 +0100

    added server for robot control

--- capsid/driver/driver.py
+++ capsid/driver/driver.py
@@ -1,17 +1,10 @@
-import smbus, time, random
-
+import smbus, time, random, copy
 from gpiozero import LED
 
-power_pin = LED(14)
+##############################################
+# i2c interface
 
 bus = smbus.SMBus(1)
-ADDR = [0x0a,
-        0x0d,
-        0x0e,
-        0x0f,
-        #0x11,
-        0x12]
-#ADDR = [0x11,0x12]
 
 def write(dev,reg,val):
     try:
@@ -22,156 +15,173 @@ def write(dev,reg,val):
 
 def read(dev,reg):
     try:
-        v = bus.read_byte_data(ADDR[dev],reg)
+        v = bus.read_byte_data(dev,reg)
         time.sleep(0.05)
         return v
     except:
         print("i2c error reading: "+str(dev))
         return 0
 
-REG_VERSION=0
-REG_ALIVE=1
-REG_ADDR=2
-REG_MODE=3
-MODE_POWER_OFF=0
-MODE_NORMAL=1
-MODE_CALI=2
-MODE_DEMO=3
-REG_LED=4
-REG_SHOW_ID=5 # 0,1,2,3 or anything else = hide all 
-REG_SERVO_SPEED=6
-REG_SERVO_INTERPOLATION=7
-REG_SERVO_START=8
-REG_EEINIT=0xfd
-REG_EESAVE=0xfe
+###################################################
+# controller for a robot virus
 
-REG_SERVO_A_ANGLE=0x08 
-REG_SERVO_A_ACTIVE=0x09 
-REG_SERVO_A_HIDE=0x0a
-REG_SERVO_A_SHOW=0x0b 
-REG_SERVO_B_ANGLE=0x0c 
-REG_SERVO_B_ACTIVE=0x0d 
-REG_SERVO_B_HIDE=0x0e
-REG_SERVO_B_SHOW=0x0f 
-REG_SERVO_C_ANGLE=0x10 
-REG_SERVO_C_ACTIVE=0x11 
-REG_SERVO_C_HIDE=0x12
-REG_SERVO_C_SHOW=0x13 
-REG_SERVO_D_ANGLE=0x14 
-REG_SERVO_D_ACTIVE=0x15 
-REG_SERVO_D_HIDE=0x16
-REG_SERVO_D_SHOW=0x17 
+class robot_virus:
+    def __init__(self,addresses):
+        self.addresses = addresses
+        # well ok rpi - I have to pretend everything is an LED?
+        self.power_pin = LED(14)
 
-default_a_hide_angle=15
-default_b_hide_angle=90
-default_c_hide_angle=0
-default_d_hide_angle=50 
 
-default_a_show_angle=60
-default_b_show_angle=45
-default_c_show_angle=45
-default_d_show_angle=10
+    REG_VERSION=0
+    REG_ALIVE=1
+    REG_ADDR=2
+    REG_MODE=3
+    MODE_POWER_OFF=0
+    MODE_NORMAL=1
+    MODE_CALI=2
+    MODE_DEMO=3
+    REG_LED=4
+    REG_SHOW_ID=5 # 0,1,2,3 or anything else = hide all 
+    REG_SERVO_SPEED=6
+    REG_SERVO_INTERPOLATION=7
+    REG_SERVO_START=8
+    REG_EEINIT=0xfd
+    REG_EESAVE=0xfe
 
-def calibrate(dev,hide,show):
-    write(dev,REG_SERVO_A_HIDE,hide[0])
-    write(dev,REG_SERVO_A_SHOW,show[0])
-    write(dev,REG_SERVO_B_HIDE,hide[1])
-    write(dev,REG_SERVO_B_SHOW,show[1])
-    write(dev,REG_SERVO_C_HIDE,hide[2])
-    write(dev,REG_SERVO_C_SHOW,show[2])
-    write(dev,REG_SERVO_D_HIDE,hide[3])
-    write(dev,REG_SERVO_D_SHOW,show[3])
-    write(dev,REG_EESAVE,1)
+    REG_SERVO_A_ANGLE=0x08 
+    REG_SERVO_A_ACTIVE=0x09 
+    REG_SERVO_A_HIDE=0x0a
+    REG_SERVO_A_SHOW=0x0b 
+    REG_SERVO_B_ANGLE=0x0c 
+    REG_SERVO_B_ACTIVE=0x0d 
+    REG_SERVO_B_HIDE=0x0e
+    REG_SERVO_B_SHOW=0x0f 
+    REG_SERVO_C_ANGLE=0x10 
+    REG_SERVO_C_ACTIVE=0x11 
+    REG_SERVO_C_HIDE=0x12
+    REG_SERVO_C_SHOW=0x13 
+    REG_SERVO_D_ANGLE=0x14 
+    REG_SERVO_D_ACTIVE=0x15 
+    REG_SERVO_D_HIDE=0x16
+    REG_SERVO_D_SHOW=0x17 
 
-def factory_reset(dev):
-    write(dev,REG_EEINIT,1)
+    default_a_hide_angle=15
+    default_b_hide_angle=90
+    default_c_hide_angle=0
+    default_d_hide_angle=50 
 
-def test_one(dev):
-    choice=0
-    while True:    
-        write(dev,REG_MODE,2)
-        time.sleep(0.1)
-        write(dev,REG_MODE,1)
-        time.sleep(0.05)
-        write(dev,REG_SHOW_ID,choice)
-        print(choice)
-        choice+=1
-        if choice>3: choice=0
-        time.sleep(1)
+    default_a_show_angle=60
+    default_b_show_angle=45
+    default_c_show_angle=45
+    default_d_show_angle=10
 
-def test_all():
-    choice=0
-    dev=0
-    while True:    
-        dev=dev+1
-        if dev>=len(ADDR):
-            dev=0
+    def calibrate(self,dev,hide,show):
+        write(dev,self.REG_SERVO_A_HIDE,hide[0])
+        write(dev,self.REG_SERVO_A_SHOW,show[0])
+        write(dev,self.REG_SERVO_B_HIDE,hide[1])
+        write(dev,self.REG_SERVO_B_SHOW,show[1])
+        write(dev,self.REG_SERVO_C_HIDE,hide[2])
+        write(dev,self.REG_SERVO_C_SHOW,show[2])
+        write(dev,self.REG_SERVO_D_HIDE,hide[3])
+        write(dev,self.REG_SERVO_D_SHOW,show[3])
+        write(dev,self.REG_EESAVE,1)
+
+    def factory_reset(self,dev):
+        write(dev,self.REG_EEINIT,1)
+
+    def test_one(self,dev):
+        choice=0
+        while True:    
+            write(dev,self.REG_MODE,2)
+            time.sleep(0.1)
+            write(dev,self.REG_MODE,1)
+            time.sleep(0.05)
+            write(dev,self.REG_SHOW_ID,choice)
+            print(choice)
             choice+=1
-        if choice>3: choice=0
+            if choice>3: choice=0
+            time.sleep(1)
 
+    def test_all(self):
+        choice=0
+        dev=0
+        while True:    
+            dev=dev+1
+            if dev>=len(self.addresses):
+                dev=0
+                choice+=1
+                if choice>3: choice=0
+
+            # clear just in case
+            write(self.addresses[dev],self.REG_MODE,2)
+            time.sleep(0.1)
+            write(self.addresses[dev],self.REG_MODE,1)
+            time.sleep(0.05)
+            write(self.addresses[dev],self.REG_SHOW_ID,random.randrange(0,5))
+            time.sleep(1)
+            
+    def flash_all(self):
+        choice=0
+        dev=0
+        while True:    
+            dev=dev+1
+            if dev>=len(self.addresses):
+                dev=0
+                choice+=1
+                if choice>1: choice=0
+            write(self.addresses[dev],self.REG_LED,choice)
+            time.sleep(0.1)
+
+
+    def cali(self):
+        self.calibrate(0x0a,[10,90,0,50],[70,40,60,0])  # <- dodgy plug a
+        self.calibrate(0x0b,[10,90,0,70],[80,30,60,10])
+        self.calibrate(0x0c,[0,90,0,50],[60,40,60,0])       
+        self.calibrate(0x0d,[10,100,0,60],[80,30,60,0]) 
+        self.calibrate(0x0e,[10,90,0,50],[80,30,60,0])
+        self.calibrate(0x0f,[10,90,0,50],[80,30,60,0])
+        self.calibrate(0x10,[10,90,0,60],[80,30,60,0]) # <- fixed 4 show distance
+        self.calibrate(0x11,[10,90,0,50],[70,40,60,0])       
+        self.calibrate(0x12,[10,90,0,50],[80,30,60,0])
+
+    def boot(self):
+        self.power_pin.on()
+        time.sleep(0.5)
+        #self.cali()
+        #turn all servos on
+        for dev in range(0,len(self.addresses)): 
+            write(self.addresses[dev],self.REG_MODE,self.MODE_NORMAL)
+            time.sleep(0.2)
+
+
+    def set_triangle(self,addr,shape_id):
         # clear just in case
-        write(ADDR[dev],REG_MODE,2)
+        write(addr,self.REG_MODE,2)
         time.sleep(0.1)
-        write(ADDR[dev],REG_MODE,1)
-        time.sleep(0.05)
-        write(ADDR[dev],REG_SHOW_ID,random.randrange(0,5))
-        time.sleep(1)
+        write(addr,self.REG_MODE,1)
+        write(addr,self.REG_SHOW_ID,shape_id)
+        print("setting "+str(addr)+" to "+str(shape_id))
 
-def flash_all():
-    choice=0
-    dev=0
-    while True:    
-        dev=dev+1
-        if dev>=len(ADDR):
-            dev=0
-            choice+=1
-        if choice>1: choice=0
-        write(ADDR[dev],REG_LED,choice)
-        time.sleep(0.1)
-
-
-def cali():
-    calibrate(0x0a,[10,90,0,50],[70,40,60,0])  # <- doge plug a
-    calibrate(0x0b,[10,90,0,70],[80,30,60,10])
-    calibrate(0x0c,[0,90,0,50],[60,40,60,0])       
-    calibrate(0x0d,[10,100,0,60],[80,30,60,0]) 
-    calibrate(0x0e,[10,90,0,50],[80,30,60,0])
-    calibrate(0x0f,[10,90,0,50],[80,30,60,0])
-    calibrate(0x10,[10,90,0,60],[80,30,60,0]) # <- fixed 4 show distance
-    calibrate(0x11,[10,90,0,50],[70,40,60,0])       
-    calibrate(0x12,[10,90,0,50],[80,30,60,0])
-
-def test_power():
-    while True:
-        power_pin.on()
-        print("on")
-        time.sleep(1)
-        power_pin.off()
-        print("off")
-        time.sleep(1)
-
-power_pin.on()
-
-# boot
-while True:
-
-    write(0x0c,0x04,1)
-    time.sleep(0.5)
-
-    write(0x0c,0x04,0)
-    time.sleep(0.5)
-
-# turn all servos on
-#for dev in range(0,len(ADDR)): 
-#    write(ADDR[dev],REG_MODE,MODE_NORMAL)
-#    time.sleep(0.2)
-
-#cali()    
-
-#calibrate(0x0b,[10,90,0,70],[80,30,60,10])
-#test_one(0x0d)
-
-#test_all()
-#flash_all()
+    def shape_to_id(self,shape_str):
+        if shape_str=="squ": return 0
+        if shape_str=="tri": return 1
+        if shape_str=="gui": return 2
+        if shape_str=="cir": return 3
+        return 4
+    
+    def distribute_shapes(self,shapes):
+        tris_per_shape = int(len(self.addresses)/len(shapes))
+        if tris_per_shape<1: tris_per_shape=1
+        tris = copy.copy(self.addresses)
+        for shape in shapes:
+            print(shape)
+            for tri_num in range(0,tris_per_shape):
+                if len(tris)>0:
+                    choice = tris[random.randrange(0,len(tris))]
+                    tris.remove(choice)                    
+                    self.set_triangle(choice,self.shape_to_id(shape))
 
 
+
+
+        
---
+++ capsid/driver/shape_server.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import SimpleHTTPServer
+import SocketServer
+import os
+import time
+from driver import robot_virus
+
+global transmitter
+
+# sudo lsof -i:8888 
+# virus = robot_virus([0x0a,
+#                      0x0d,
+#                      0x0e,
+#                      0x0f,
+#                      #0x11,
+#                      0x12])
+
+virus = robot_virus([0x0c])
+
+virus.boot()
+
+class server(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    
+    def do_POST(self):
+        if self.path=="/ping":
+            self.send_response(200)
+            return
+
+        if self.path=="/shapes":
+            data_len = int(self.headers['Content-Length'])
+            data_string = self.rfile.read(data_len)
+            print("data_string is: "+data_string)
+            virus.distribute_shapes(data_string.split(' '))
+            self.send_response(200)
+            return
+
+        self.send_response(501)
+        
+    def do_GET(self):
+        return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+    
+PORT = 8889
+SocketServer.TCPServer.allow_reuse_address = True
+httpd = SocketServer.TCPServer(("", PORT), server)
+httpd.serve_forever()
+
